42dbc1ec4791a44711e75944d722f2013deb7802
Konrad Gadzina <konradgadzina@MacBook-Pro-Konrad.local>
2017-06-13 22:38:22 +0200

    Add project with moving and camera centering

---
+++ Assets/Scripts/CenterViewBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterViewBehaviour : MonoBehaviour
+{
+
+    public GameObject followedObject;
+    public Vector3 distance = new Vector3(0, -3.0f, 5.0f);
+    public Vector3 rotation = new Vector3(3.0f, 0, 0);
+
+    public bool isLocked = false;
+    public const string activatorButton = "space";
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.localRotation = new Quaternion(0, 0, 0, 0);
+        transform.Rotate(rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(activatorButton))
+        {
+            isLocked = !isLocked;
+        }
+
+        if (isLocked)
+        {
+            transform.localPosition = followedObject.transform.position - distance;
+        }
+
+    }
+}
---
+++ Assets/Scripts/MoveBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBehaviour : MonoBehaviour {
+
+    Rigidbody capsuleRb;
+    public float speedMultiplier = 1;
+
+	// Use this for initialization
+	void Start () {
+        capsuleRb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(horizontalInput, 0.0f, verticalInput);
+
+        capsuleRb.AddForce(movementVector * speedMultiplier);
+	}
+}
